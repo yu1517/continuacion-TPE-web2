8873933a6771d5f47799a18a10be0dce963d0dd9
yuli <giuliana.delatorre@hotmail.com>
2022-11-07 16:47:13 -0300

    actualizando tp con api

--- api/apiBookController.php
+++ api/apiBookController.php
@@ -6,10 +6,12 @@ require_once './api/apiView.php';
 class ApiBookController {
     private $model;
     private $view;
+    private $data;
 
     function __construct(){
         $this->model = new BookModel();
         $this->view = new ApiView();
+        $this->data = file_get_contents('php://input');
     }
 
     public function getBooks($params = null){
@@ -40,4 +42,22 @@ class ApiBookController {
             $this->view->response("El libro con el id=$id no existe", 404);
     }
 
+    private function getData(){
+        return json_decode($this->data);
+    }
+
+    public function addBook($params = null){
+        $data = $this->getData();
+
+        $title = $data->$title;
+        $genre = $data->$genre;
+        $author = $data->$id_author;
+
+        $id = $this->model->insertBook($data->$title, $data->$genre, $data->$id_author);
+        $book = $this->model->getBook($id);
+        if($book)
+            $this->view->response($book, 200);
+        else
+            $this->view->response("El libro no se pudo crear", 500);
+    }
 }
--- router-api.php
+++ router-api.php
@@ -9,6 +9,7 @@ $router = new Router();
 $router->addRoute('books', 'GET', 'ApiBookController', 'getBooks');
 $router->addRoute('books/:ID', 'GET', 'ApiBookController', 'getBook');
 $router->addRoute('books/:ID', 'DELETE', 'ApiBookController', 'removeBook');
+$router->addRoute('books', 'POST', 'ApiBookController', 'addBook');
 
 // ejecuta la ruta (sea cual sea)
 $router->route($_GET["resource"], $_SERVER['REQUEST_METHOD']);
